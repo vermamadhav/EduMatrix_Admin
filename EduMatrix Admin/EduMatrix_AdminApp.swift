637061c03a85511da6dc2ad3ac6353681ed6c845
Madhav Verma <madhavverma.hll@gmail.com>
2024-07-03 16:21:16 +0530

    Initial Commit

---
+++ EduMatrix Admin/EduMatrix_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduMatrix_AdminApp.swift
+//  EduMatrix Admin
+//
+//  Created by Madhav Verma on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct EduMatrix_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
